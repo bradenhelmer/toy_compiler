3fb0accf10ee99149c0dc21efdc1cac4cdc41a41
Braden Helmer <bradenhelmeraus@gmail.com>
2023-02-22 12:39:10 -0500

    Revert "Revert "all codegen methods except variable decl""
    This reverts commit b8fc2c6853af50de53b72c60e436978d692b5e87.

--- codegen.cpp
+++ codegen.cpp
@@ -5,7 +5,7 @@
 void CodeGenContext::generateCode(NBlock &root) {}
 llvm::GenericValue CodeGenContext::runCode() {}
 
-static const llvm::Type *typeOf(const NIdentifier &type) {
+static llvm::Type *typeOf(const NIdentifier &type) {
   if (type.name.compare("int") == 0) {
     return llvm::Type::getInt64Ty(CodeGenContext::getGlobalContext());
   } else if (type.name.compare("double") == 0) {
@@ -26,11 +26,100 @@ llvm::Value *NDouble::codeGen(CodeGenContext &context) {
       llvm::Type::getDoubleTy(CodeGenContext::getGlobalContext()), value);
 }
 
-llvm::Value *NIdentifier::codeGen(CodeGenContext &context) {}
-llvm::Value *NMethodCall::codeGen(CodeGenContext &context) {}
-llvm::Value *NBinaryOperator::codeGen(CodeGenContext &context) {}
-llvm::Value *NAssignment::codeGen(CodeGenContext &context) {}
-llvm::Value *NBlock::codeGen(CodeGenContext &context) {}
-llvm::Value *NExpressionStatement::codeGen(CodeGenContext &context) {}
+llvm::Value *NIdentifier::codeGen(CodeGenContext &context) {
+  std::cout << "Creating identifier reference: " << name << std::endl;
+  if (context.locals().find(name) == context.locals().end()) {
+    std::cerr << "Undeclared variable" << name << std::endl;
+    return NULL;
+  }
+  llvm::Value *V = context.locals()[name];
+  return new llvm::LoadInst(V->getType(), V, "", false, context.currentBlock());
+}
+
+llvm::Value *NMethodCall::codeGen(CodeGenContext &context) {
+  llvm::Function *FUNCTION = context.module->getFunction(id.name.c_str());
+  std::vector<llvm::Value *> args;
+  ExpressionList::const_iterator it;
+  for (it = arguments.begin(); it != arguments.end(); it++) {
+    args.push_back((**it).codeGen(context));
+  }
+  std::cout << "Creating method call: " << id.name << std::endl;
+  llvm::CallInst *CALL =
+      llvm::CallInst::Create(FUNCTION->getFunctionType(), FUNCTION, args,
+                             id.name, context.currentBlock());
+  return CALL;
+}
+
+llvm::Value *NBinaryOperator::codeGen(CodeGenContext &context) {
+  std::cout << "Creating binary operation : " << op << std::endl;
+  llvm::Instruction::BinaryOps instr;
+  switch (op) {
+  case TPLUS:
+    instr = llvm::Instruction::Add;
+    goto math;
+  case TMINUS:
+    instr = llvm::Instruction::Sub;
+    goto math;
+  case TMUL:
+    instr = llvm::Instruction::Mul;
+    goto math;
+  case TDIV:
+    instr = llvm::Instruction::SDiv;
+    goto math;
+  }
+  return NULL;
+math:
+  return llvm::BinaryOperator::Create(instr, lhs.codeGen(context),
+                                      rhs.codeGen(context), "",
+                                      context.currentBlock());
+}
+llvm::Value *NAssignment::codeGen(CodeGenContext &context) {
+  std::cout << "Creating assignment for " << lhs.name << std::endl;
+  if (context.locals().find(lhs.name) == context.locals().end()) {
+    std::cerr << "Undeclared variable" << lhs.name << std::endl;
+    return NULL;
+  }
+  llvm::Value *LHSVAL = context.locals()[lhs.name];
+  return new llvm::StoreInst(rhs.codeGen(context), LHSVAL, false,
+                             context.currentBlock());
+}
+llvm::Value *NBlock::codeGen(CodeGenContext &context) {
+  StatementList::const_iterator it;
+  llvm::Value *LAST = NULL;
+  for (it = statements.begin(); it != statements.end(); it++) {
+    std::cout << "Generating code for " << typeid(**it).name() << std::endl;
+    LAST = (**it).codeGen(context);
+  }
+  std::cout << "Creating block..." << std::endl;
+  return LAST;
+}
+llvm::Value *NExpressionStatement::codeGen(CodeGenContext &context) {
+  std::cout << "Generating code for " << typeid(expression).name() << std::endl;
+  return expression.codeGen(context);
+}
 llvm::Value *NVariableDeclaration::codeGen(CodeGenContext &context) {}
-llvm::Value *NFunctionDeclaration::codeGen(CodeGenContext &context) {}
+
+llvm::Value *NFunctionDeclaration::codeGen(CodeGenContext &context) {
+  std::vector<llvm::Type *> argTypes;
+  VariableList::const_iterator it;
+  for (it = arguments.begin(); it != arguments.end(); it++) {
+    argTypes.push_back(typeOf((**it).type));
+  }
+  llvm::FunctionType *FTYPE =
+      llvm::FunctionType::get(typeOf(type), argTypes, false);
+  llvm::Function *FUNCTION =
+      llvm::Function::Create(FTYPE, llvm::GlobalValue::InternalLinkage,
+                             id.name.c_str(), context.module);
+  llvm::BasicBlock *BBBLOCK = llvm::BasicBlock::Create(
+      CodeGenContext::getGlobalContext(), "entry", FUNCTION, 0);
+  context.pushBlock(BBBLOCK);
+
+  for (it = arguments.begin(); it != arguments.end(); it++) {
+    (**it).codeGen(context);
+  }
+  block.codeGen(context);
+  llvm::ReturnInst::Create(CodeGenContext::getGlobalContext(), BBBLOCK);
+  context.popBlock();
+  std::cout << "Creating function: " << id.name << std::endl;
+  return FUNCTION;
+}
