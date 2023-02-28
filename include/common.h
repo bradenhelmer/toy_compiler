07d0fd562d18d357334fe0636acea808c4c13f17
Braden Helmer <bradenhelmeraus@gmail.com>
2023-02-28 07:58:13 -0500

    forgot to add to git

---
+++ include/common.h
@@ -0,0 +1,8 @@
+// common.h
+// ~~~~~~~~
+// Common macro definitions
+
+#ifndef TEST_OUT
+#include <iostream>
+#define TEST_OUT(X) std::cout << X << std::endl;
+#endif
---
+++ include/token_buff.h
@@ -0,0 +1,22 @@
+// token_buff.h
+// ~~~~~~~~~~~~
+// Defines the token buffer
+#ifndef TOKEN_BUFF_H
+#define TOKEN_BUFF_H
+#include <cstring>
+
+class TokenBuffer {
+public:
+    TokenBuffer();
+    ~TokenBuffer();
+
+    void append(char c);
+    const char* str() const;
+
+private:
+    char* buffer_;
+    size_t size_;
+    size_t length_;
+};
+
+#endif
